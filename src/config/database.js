3c47e630715ab424cbc9a262045572e8575be2cf
80783783191 <marcelo.marques@planejamento.gov.br>
2020-02-06 09:30:24 -0300

    deply heroku

--- src/config/database.js
+++ src/config/database.js
@@ -8,7 +8,7 @@ module.exports = {
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   dialect: process.env.DB_DIALECT || 'postgres',
-  storage: './__tests__/database.sqlite',
+  storage: process.env.DB_SQLITE_STORAGE || './__tests__/database.sqlite',
   //operatorsAliases: 0, // false
   //logging: false,
   define: {
